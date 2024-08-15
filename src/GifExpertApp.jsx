dadaf8f946a8e3a9a809751320eadc2826405c9c
isa <isaurini1902@gmail.com>
2024-08-14 20:23:29 -0600

    add category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Resident Evil", "Silent Hill", "Dead Space"]);
+  const onAddCategory=()=>{
+    // console.log("Gears of War");
+    setCategories([...categories, "Gears of War"]);
+  }
   return (
     <>
-        <h1>Gif expert app</h1>
+      <h1>Gif expert app</h1>
+      <input type="text" placeholder="Ingrese Una Categoria" />
+      <button onClick={onAddCategory}>Add</button>
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   )
 }
